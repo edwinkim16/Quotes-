9a10348bbfc7f33ff739b8e9c8d0ce99b8bbd08b
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-08-22 19:19:50 +0300

    debugging

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -7,8 +7,10 @@ export class DateCountPipe implements PipeTransform {
 
   transform(value: any, ...args: any[]): number {
     let today:Date = new Date(); 
-    let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
-    var dateDifference = Math.abs(value - todayWithNoTime) 
+    let todayWithNoTime:any = new Date(today.getFullYear(), 
+    today.getMonth(), today.getDate())
+    var dateDifference = Math.abs(todayWithNoTime -value) 
+    console.log(dateDifference);
     const secondsInDay = 86400; 
     var dateDifferenceSeconds = dateDifference*0.001; 
     var dateCounter = dateDifferenceSeconds/secondsInDay;
