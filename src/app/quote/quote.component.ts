ee5f3fd9985dc209252a6cb432a41aad64ada08f
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-08-22 15:12:20 +0300

    created directive and addition of dates

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,13 +8,19 @@ import { Quotes } from '../quotes';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quotes[]=[
-    new Quotes("Hello world","This is my first program","Edwin"),
-    new Quotes("Chalk circle","You who have listened to story of chalk circle","Bertolt Brecht"),
-    new Quotes("Define world","We have 195 countries","Wikipedia"),
+    new Quotes("Hello world","This is my first program","Edwin",new Date(2021,9,24)),
+    new Quotes("Chalk circle","You who have listened to story of chalk circle","Bertolt Brecht",new Date(2030,1,14)),
+    new Quotes("Define world","We have 195 countries","Wikipedia",new Date(2021,1,8)),
   ];
 
   toggleDetails(index:any){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+
+  completeGoal(isComplete:any, index:any){
+    if (isComplete) {
+      this.quotes.splice(index,1);
+    }
   }
  
   constructor() { }
--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,7 +1,7 @@
 export class Quotes {
    
     showDescription: boolean;
-  constructor(public name:string ,public description: string,public author:string
+  constructor(public name:string ,public description: string,public author:string,public completeDate: Date
      ){
         this.showDescription=false;
   }
