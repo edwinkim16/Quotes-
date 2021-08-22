9e3a9a7b678410e2bfcb0e60957061baaff4286d
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-08-22 11:01:23 +0300

    created upvote and downvote property

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,7 @@
 export class Quotes {
     showDescription: boolean;
-  constructor(public author:string,public name: string,public description: string,public completeDate: Date){
+  constructor(public author:string,public name: string,public description: string,public completeDate: Date
+    ,public upVote=0,public downVote=0 ){
     this.showDescription=false;
   }
 }
