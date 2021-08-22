5b05e961696b7821c256575dfb44946b68f20f59
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-08-22 16:09:34 +0300

    data binding of form

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quotes } from '../quotes';
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+  newQuote = new Quotes("","","",new Date());
 
   constructor() { }
 
