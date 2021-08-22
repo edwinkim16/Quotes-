7b507c3f932a2002ac72962c153ff65d5bfb8be5
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-08-22 10:59:33 +0300

    created properties

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,6 @@
 export class Quotes {
+    showDescription: boolean;
+  constructor(public author:string,public name: string,public description: string,public completeDate: Date){
+    this.showDescription=false;
+  }
 }
